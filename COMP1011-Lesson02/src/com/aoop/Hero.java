b0d0c3ae4f10101538268e9a15f678403e6c99b1
OscarAng <98577480+OscarAng@users.noreply.github.com>
2022-01-29 23:51:14 +0800

    Updated

--- COMP1011-Lesson02/src/com/aoop/Hero.java
+++ COMP1011-Lesson02/src/com/aoop/Hero.java
@@ -25,16 +25,32 @@ public class Hero {
 	// Private Methods
 	private void generateAbilities() {
 		
-		
+		this.strength = ( int ) ( Math.random() * 100 + 1 );
+		this.speed = ( int ) ( Math.random() * 100 + 1 );
+		this.health = ( int ) ( Math.random() * 100 + 1 );
 		
 	}
 	
 	// Public Methods
+	public void showAbilities() {
+		
+		System.out.println( "-----------------------------" );
+		System.out.printf( "Strength: %d", this.strength );
+		System.out.printf( "Speed: %d", this.speed );
+		System.out.printf( "Health: %d", this.health );
+		System.out.println( "-----------------------------" );
+		
+	}
+	
 	public void fight() {
+		
+		System.out.printf( "%s is Fighting", this.name );
 		
 	}
 	
 	public void run() {
+		
+		System.out.printf( "%s is Running", this.name );
 		
 	}
 	
