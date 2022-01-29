b1ff9358319109a6b5ca68b04e6b9799948d4734
OscarAng <98577480+OscarAng@users.noreply.github.com>
2022-01-29 23:39:54 +0800

    Updated

--- COMP1011-Lesson02/src/com/aoop/Hero.java
+++ COMP1011-Lesson02/src/com/aoop/Hero.java
@@ -2,4 +2,40 @@ package com.aoop;
 
 public class Hero {
 
+	// Private Properties
+	private int strength, speed, health;
+	
+	// Public Properties
+	public String name;
+	
+	// Getters
+	public int getStrength() { return strength; }
+	public int getSpeed() { return speed; }
+	public int getHealth() { return health; }
+
+	// Constructor
+	public Hero( String name ) {
+		
+		this.name = name;
+		
+		generateAbilities();
+		
+	}
+	
+	// Private Methods
+	private void generateAbilities() {
+		
+		
+		
+	}
+	
+	// Public Methods
+	public void fight() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
 }
