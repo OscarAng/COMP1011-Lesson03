8b924f0d77c81578760a70a6d0fb68ca31d6054e
OscarAng <98577480+OscarAng@users.noreply.github.com>
2022-01-30 20:32:12 +0800

    Instantiated New Hero

--- COMP1011-Lesson02/src/com/aoop/Project.java
+++ COMP1011-Lesson02/src/com/aoop/Project.java
@@ -4,6 +4,12 @@ public class Project {
 
 	public static void main( String[] args ) {
 		
+		// Instantiate Hero
+		Hero hero = new Hero( "Big Man" );
+		hero.fight();
+		hero.run();
+		hero.showAbilities();
+		
 	}
 	
 }
